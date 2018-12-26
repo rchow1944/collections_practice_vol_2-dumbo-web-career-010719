ca8fc79794f3a44c18fb37d38aab0f798839198f
Raymond Chow <raychow1944@yahoo.com>
2018-12-26 08:49:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
 # your code goes here
+def begins_with_r(arr)
+  res = true
+  arr.find do |tool|
+    if tool[0] != 'r'
+      res = false
+    end
+  end
+  res
+end
+
+def contain_a(arr)
+  # res = []
+  # arr.each do |word|
+  #   if word.include?('a')
+  #     res << word
+  #   end
+  # end
+  # res
+  arr.select do |word|
+    word.include?('a')
+  end
+end
+
+def first_wa(arr)
+  arr.find do |word|
+    word[0..1] == 'wa'
+  end
+end
+
+def remove_non_strings(arr)
+  arr.delete_if do |e| 
+    !(e.is_a? String)
+  end
+end
+
+def count_elements(arr)
+  curr_hash = {}
+  arr.each do |el|
+    binding.pry
+    arr.count(el)
+  end
+end
+
+
+
+
+
+
+
